db41d15cd7452e24fd745452c3275e9c8e7f4bc8
manzeelanepali <nepalimanzeela@gmail.com>
2022-07-05 20:48:42 +0545

    my includes

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+  let result = false;
+  for (let i = 0; i < array.length; i++) {
+    let currValue = array[i];
+    if (currValue === searchValue) {
+      return true;
+    }
+  }
+  return result;
+}
