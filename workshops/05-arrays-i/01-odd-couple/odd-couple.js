d028fa3da0cba1928a0ddf91e3d0a0ac04f5be7d
manzeelanepali <nepalimanzeela@gmail.com>
2022-07-05 20:39:01 +0545

    odd couples

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+// oddCouple([1, 2, 3, 4, 5]); // => [1, 3]
+// oddCouple([10, 15, 20]); // => [15]
+// oddCouple(2, 4, 6, 8); // => []
+
+function oddCouple(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let currValue = array[i];
+    // if (currValue % 2 === 0) {
+    //   return result;
+    // } else
+    if (currValue % 2 === 1) {
+      result.push(currValue);
+    }
+    if (result.length === 2) {
+      break;
+    }
+  }
+  return result;
+}
