378c081d6a6c870cf2e2a336ef6bd1c67ec2dca6
manzeelanepali <nepalimanzeela@gmail.com>
2022-07-05 22:31:55 +0545

    my includes

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,11 +1,14 @@
 // YOUR CODE BELOW
 function myIncludes(array, searchValue) {
-  let result = false;
+  debugger;
+
   for (let i = 0; i < array.length; i++) {
     let currValue = array[i];
     if (currValue === searchValue) {
       return true;
     }
   }
-  return result;
+  return false;
 }
+
+myIncludes([1, 2, 7], 3);
--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+// myLastIndexOf(['gee', 'gee', 'gee', 'gee', 'baby', 'baby'], 'gee'); // => 3
+// If the searchValue isn't in the array, `myLastIndexOf` should return -1.
+
+// ```javascript
+// myLastIndexOf(['Tiffany', 'Sunny', 'Yoona'], 'Jessica'); // => -1
+// ```
+
+// If startIdx is defined, `myLastIndexOf` should start looking for the seachValue
+// at that index, and then move toward the front of the array as it looks for the
+// value.
+
+// ```javascript
+// myLastIndexOf(['the', 'girls', 'bring', 'the', 'boys', 'out'], 'the', 2); // => 0
+// ```
+function myLastIndexOf(array, searchValue) {
+  let result = -1;
+  for (let i = 0; i < array.length; i++) {
+    let currValue = array[i];
+    if (searchValue === currValue) {
+      return searchValue[i];
+    }
+  }
+  return result;
+}
